de0f24761081278e75abde0aa9e19f91f22b68ae
sanduirina19 <sanduirina19@stud.ase.ro>
2022-03-02 19:11:00 +0200

    Added classes from seminar 2

---
+++ s2/src/clase/Reader.java
@@ -0,0 +1,9 @@
+package clase;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public interface Reader {
+    public List<Aplicant> readAplicanti(String file) throws FileNotFoundException;
+
+}
